870133c44a20b766fe503a0bf1e3a184c21f064b
Donutellko <donutellko@gmail.com>
2017-09-05 18:13:35 +0300

    Initial commit: echo-bot.

---
+++ src/main/java/DonutellkoBot.java
@@ -0,0 +1,44 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by donat on 8/28/17.
+ */
+
+public class DonutellkoBot extends TelegramLongPollingBot {
+    public static TelegramLongPollingBot donutellkoBot;
+
+    public DonutellkoBot() {
+        super();
+        donutellkoBot = this;
+    }
+
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "DonutellkoBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "424429240:AAHSc651dDwaUM_hbhiFjtG5jga9Ya7PFKg";
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by donat on 9/5/17.
+ */
+public class Main {
+    public static void main (String[] args) {
+
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new DonutellkoBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
